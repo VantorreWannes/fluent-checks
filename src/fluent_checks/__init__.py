9a7b636a3f5f1f1c088ae94d11094c84a66d86fc
vantorrewannes <vantorrewannes@gmail.com>
2025-08-27 21:11:56 +0200

    feature: Implement Delayed, Repeating and Looping checks.

--- src/fluent_checks/__init__.py
+++ src/fluent_checks/__init__.py
@@ -1,5 +1,7 @@
 from abc import ABC
-from typing import Callable, Self, final
+from itertools import repeat
+import time
+from typing import Callable, Self, final, override
 
 type Condition = Callable[[], bool]
 
@@ -7,11 +9,26 @@ type Condition = Callable[[], bool]
 class Check(ABC):
     def __init__(self, condition: Condition) -> None:
         super().__init__()
-        self.condition = condition
+        self._condition = condition
 
     @final
     def as_bool(self) -> bool:
         return bool(self)
+
+    def with_delay(self, delay: float) -> "DelayedCheck":
+        return DelayedCheck(self, delay)
+
+    def sometimes(self) -> "LoopingOrCheck":
+        return LoopingOrCheck(self)
+
+    def always(self) -> "LoopingOrCheck":
+        return LoopingOrCheck(self)
+
+    def succeeds_within(self, times: int) -> "RepeatingAndCheck":
+        return RepeatingAndCheck(self, times)
+
+    def is_consistent_for(self, times: int) -> "RepeatingOrCheck":
+        return RepeatingOrCheck(self, times)
 
     def __and__(self, other: Self) -> "Check":
         return AndCheck(self, other)
@@ -23,7 +40,7 @@ class Check(ABC):
         return InvertedCheck(self)
 
     def __bool__(self) -> bool:
-        return self.condition()
+        return self._condition()
 
     def __repr__(self) -> str:
         return f"Check({self.as_bool()})"
@@ -31,46 +48,84 @@ class Check(ABC):
 
 class AllCheck(Check):
     def __init__(self, *checks: Check) -> None:
-        super().__init__(lambda: all(checks))
-        self.checks = checks
+        super().__init__(condition=lambda: all(checks))
+        self._checks: tuple[Check, ...] = checks
 
     def __repr__(self) -> str:
-        return " and ".join([check.__repr__() for check in self.checks])
+        return " and ".join([check.__repr__() for check in self._checks])
 
 
 class AnyCheck(Check):
     def __init__(self, *checks: Check) -> None:
-        super().__init__(lambda: any(checks))
-        self.checks = checks
+        super().__init__(condition=lambda: any(checks))
+        self._checks: tuple[Check, ...] = checks
 
     def __repr__(self) -> str:
-        return " or ".join([check.__repr__() for check in self.checks])
+        return " or ".join([check.__repr__() for check in self._checks])
 
 
 class AndCheck(AllCheck):
     def __init__(self, left: Check, right: Check) -> None:
         super().__init__(*[left, right])
-        self.left: Check = left
-        self.right: Check = right
+        self._left: Check = left
+        self._right: Check = right
 
     def __repr__(self) -> str:
-        return f"{self.left.__repr__()} or {self.right.__repr__()}"
+        return f"{self._left.__repr__()} or {self._right.__repr__()}"
 
 
 class OrCheck(AnyCheck):
     def __init__(self, left: Check, right: Check) -> None:
         super().__init__(*[left, right])
-        self.left: Check = left
-        self.right: Check = right
+        self._left: Check = left
+        self._right: Check = right
 
     def __repr__(self) -> str:
-        return f"{self.left.__repr__()} or {self.right.__repr__()}"
+        return f"{self._left.__repr__()} or {self._right.__repr__()}"
 
 
 class InvertedCheck(Check):
     def __init__(self, check: Check) -> None:
-        super().__init__(lambda: not check)
-        self.check = check
+        super().__init__(condition=lambda: not check)
+        self._inverted: Check = check
 
     def __repr__(self) -> str:
         return f"not {super().__repr__()}"
+
+
+class DelayedCheck(Check):
+    def __init__(self, check: Check, delay: float) -> None:
+        super().__init__(condition=lambda: bool(check))
+        self._check: Check = check
+        self._delay: float = delay
+
+    @override
+    def __bool__(self) -> bool:
+        time.sleep(self._delay)
+        return self._check.as_bool()
+
+
+class RepeatingAndCheck(AllCheck):
+    def __init__(self, check: Check, times: int) -> None:
+        super().__init__(*repeat[Check](check, times))
+        self._check: Check = check
+        self._times: int = times
+
+
+class RepeatingOrCheck(AnyCheck):
+    def __init__(self, check: Check, times: int) -> None:
+        super().__init__(*repeat[Check](check, times))
+        self._check: Check = check
+        self._times: int = times
+
+
+class LoopingAndCheck(AllCheck):
+    def __init__(self, check: Check) -> None:
+        super().__init__(*repeat[Check](check))
+        self._check: Check = check
+
+
+class LoopingOrCheck(AnyCheck):
+    def __init__(self, check: Check) -> None:
+        super().__init__(*repeat[Check](check))
+        self._check: Check = check
